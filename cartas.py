46d448b6139f652ebeb499439420620c423f012e
PedroLucaspllgc <161877989+PedroLucaspllgc@users.noreply.github.com>
2024-03-20 16:50:45 -0300

    Add files via upload

---
+++ cartas.py
@@ -0,0 +1,10 @@
+class Carta:
+    def __init__(self, valor, naipe):
+        self.valor = valor
+        self.naipe = naipe
+
+    def __str__(self):
+        return f"{self.valor} de {self.naipe}"
+
+carta_exemplo = Carta("Ás", "Espadas")
+print(carta_exemplo)
