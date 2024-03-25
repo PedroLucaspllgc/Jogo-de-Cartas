206697086410df404f9839b5f2d91b9027babcd5
matheusghb <161875342+matheusghb@users.noreply.github.com>
2024-03-25 16:40:50 -0300

    Add files via upload

---
+++ jogador.py
@@ -0,0 +1,15 @@
+from cartas import Carta
+
+class Jogador:
+    def __init__ (self, nome, mao):
+        self.nome = nome
+        self.mao = []
+        self.mao = mao
+
+    def recebercar(self):
+        return mao.append(f'{self.valor}, {self.naipe}')
+
+    def exibircar(self):
+        return print (mao)
+
+
